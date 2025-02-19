c7e4e1d854618d14971f1eec31931e3a5a9290e1
Mochalov Maksim <mmochalov@usetech.ru>
2025-02-19 03:46:38 +0300

    used mozaika

--- src/hooks/useScore.ts
+++ src/hooks/useScore.ts
@@ -26,6 +26,7 @@ export const useScore = (): Render => {
   canvas.height = count
 
   img.onload = () => {
+    const progress = 7204
     context?.drawImage(img, 0, 0)
     const cols = []
     const diff = 100 / count
@@ -42,37 +43,68 @@ export const useScore = (): Render => {
       const { s, cx, cy, m } = useMeasure()
 
       popits = []
-      const w = Math.round((s * 0.7) / count)
+      const w = Math.round((s * 0.8 - count + 1) / count)
 
-      const ws = Math.round(cx - w * (count / 2))
-      const hs = Math.round(cy - w * (count / 2))
+      const ws = Math.round(cx - w * (count / 2) - (count - 1) / 2)
+      const hs = Math.round(cy - w * (count / 2) - (count - 1) / 2)
 
       for (let i = 0; i < count; i++) {
         for (let j = 0; j < count; j++) {
           popits.push(Pixel({
-            x: ws + j * w + w,
-            y: hs + i * w + w,
+            x: ws + j * w + w + j,
+            y: hs + i * w + w + i,
             w,
-            c: cols[j][i],
-            // c: i > 43 && j > 29 ? '#000000' : cols[j][i]
+            // c: cols[j][i],
+            // c: i * 100 + j > progress ? '#222' : cols[j][i]
+            c: rand(1) < 1 ? cols[j][i] : '#222'
           }))
         }
       }
 
       back = () => {
-        ctx.strokeStyle = shade('#ffeb3b', 10)
-        ctx.lineCap = 'round'
-        ctx.lineWidth = 2 * m
+        ctx.fillStyle = '#000'
         ctx.beginPath()
-        ctx.moveTo(ws, hs)
-        ctx.lineTo(ws + count * w, hs)
-        ctx.stroke()
+        ctx.rect(ws + w, hs + w, count * w + count - 1, count * w + count - 1)
+        ctx.closePath()
+        ctx.fill()
       }
+
+      // back = Popit({
+      //   x: cx,
+      //   y: cy,
+      //   r: Math.round(s * 0.45),
+      //   c: '#ffeb3b',
+      //   p: true,
+      // })
+      // back = () => {
+      //   ctx.strokeStyle = '#000000'
+      //   ctx.lineCap = 'round'
+      //   ctx.lineWidth = 2 * m
+      //   ctx.beginPath()
+      //   ctx.moveTo(ws, hs)
+      //   ctx.lineTo(ws + count * w, hs)
+      //   ctx.stroke()
+
+      //   ctx.beginPath()
+      //   ctx.moveTo(ws + count * w, hs)
+      //   ctx.lineTo(ws + count * w, hs + count * w)
+      //   ctx.stroke()
+
+      //   ctx.beginPath()
+      //   ctx.moveTo(ws + count * w, hs + count * w)
+      //   ctx.lineTo(ws, hs + count * w)
+      //   ctx.stroke()
+  
+      //   ctx.beginPath()
+      //   ctx.moveTo(ws, hs + count * w)
+      //   ctx.lineTo(ws, hs)
+      //   ctx.stroke()
+      // }
     })
   }
 
   return () => {
+    back?.()
     popits.forEach(p => p())
-    // back?.()
   }
 }
