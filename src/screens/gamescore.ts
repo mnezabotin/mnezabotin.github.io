aa1c40d7450b5a4867b37470b7ccafb6ec89f047
Mochalov Maksim <mmochalov@usetech.ru>
2025-05-23 18:37:05 +0300

    fixes

--- src/screens/gamescore.ts
+++ src/screens/gamescore.ts
@@ -10,20 +10,20 @@ export const GameScore = (): Render => {
 
   const { render: play } = usePlayButton({
     getX: (x, s, m) => innerWidth > innerHeight ?
-      x + (s - 2 * s * 0.055 * 1.6) / 2 + 4 * m + Math.round(s * 0.1) :
+      Math.min(x + (s - 2 * s * 0.055 * 1.6) / 2 + 4 * m + Math.round(s * 0.1), innerWidth - 2 * m - Math.round(s * 0.1)) :
       x + (s - 2 * s * 0.055 * 1.6) / 8 + 0 *  Math.round(s * 0.1) + 2 * m,
     getY: (y, s, m) => innerWidth > innerHeight ?
       y + (s - 2 * s * 0.055 * 1.6) / 2 + 0 * m - Math.round(s * 0.1) :
-      y + (s - 2 * s * 0.055 * 1.6) / 2 + 4 * m + Math.round(s * 0.1)
+      Math.min(y + (s - 2 * s * 0.055 * 1.6) / 2 + 4 * m + Math.round(s * 0.1), innerHeight - 2 * m - Math.round(s * 0.1))
   })
 
   const { render: pause } = usePauseButton({
     getX: (x, s, m) => innerWidth > innerHeight ?
-      x - (s - 2 * s * 0.055 * 1.6) / 2 - 4 * m - Math.round(s * 0.1) :
+      Math.max(x - (s - 2 * s * 0.055 * 1.6) / 2 - 4 * m - Math.round(s * 0.1), 2 * m + Math.round(s * 0.1)) :
       x + (s - 2 * s * 0.055 * 1.6) / 8 - 2 * Math.round(s * 0.1) - 2 * m,
     getY: (y, s, m) => innerWidth > innerHeight ?
       y - (s - 2 * s * 0.055 * 1.6) / 2 + 0 * m + Math.round(s * 0.1) :
-      y + (s - 2 * s * 0.055 * 1.6) / 2 + 4 * m + Math.round(s * 0.1)
+      Math.min(y + (s - 2 * s * 0.055 * 1.6) / 2 + 4 * m + Math.round(s * 0.1), innerHeight - 2 * m - Math.round(s * 0.1))
   })
 
   plusDifficulty()
