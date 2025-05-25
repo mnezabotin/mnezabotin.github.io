8bd1e5721b6d8d5d63385a0e4a8521b0582675e3
Mochalov Maksim <mmochalov@usetech.ru>
2025-05-25 16:37:35 +0300

    new pop effects

--- src/hooks/effects/usePopEffect.ts
+++ src/hooks/effects/usePopEffect.ts
@@ -2,7 +2,7 @@ import { Props as PptProps } from '@/shapes/popit'
 import { useWebcore } from '@/webcore'
 import { Render } from '@/webcore/types'
 
-const RANGE = 250 / 2
+const RANGE = 100 / 2
 
 type PopEffect = {
   popEffect: (p: PptProps) => void
@@ -10,13 +10,19 @@ type PopEffect = {
 }
 
 export const usePopEffect = (): PopEffect => {
-  const { loop, ctx, loopStop, shade } = useWebcore()
+  const {
+    loop,
+    loopStop,
+    ctx,
+    shade
+  } = useWebcore()
 
   const effects: Record<string, {
     loopEffect: () => void
     tic: number,
     dir: number,
     popit: PptProps
+    origR: number
   }> = { }
 
   const loopEffect = (key: string) => {
@@ -25,15 +31,19 @@ export const usePopEffect = (): PopEffect => {
       return
     }
 
+    const p = e.popit
+
     e.tic += RANGE / 5
 
     if (e.tic >= RANGE) {
       e.dir = -1
     }
+    p.r = e.origR + (e.origR * 0.00075 * (e.tic + (e.dir > 0 ? 0 : RANGE - e.tic)))
 
     if (e.tic >= 2 * RANGE) {
       loopStop(e.loopEffect)
       delete effects[key]
+      p.r = e.origR
     }
   }
 
@@ -44,7 +54,7 @@ export const usePopEffect = (): PopEffect => {
     const tic = 1
     const dir = 1
   
-    effects[key] = { loopEffect: lpEffect, tic, dir, popit: p }
+    effects[key] = { loopEffect: lpEffect, tic, dir, popit: p, origR: p.r }
 
     loop(lpEffect)
   }
@@ -60,7 +70,7 @@ export const usePopEffect = (): PopEffect => {
       ctx.arc(
         p.x,
         p.y,
-        p.r + (p.r * 0.001 * (e.tic + (e.dir > 0 ? 0 : RANGE - e.tic))),
+        p.r + (e.origR * 0.001 * (e.tic + (e.dir > 0 ? 0 : RANGE - e.tic))),
         0,
         Math.PI * 2
       )
