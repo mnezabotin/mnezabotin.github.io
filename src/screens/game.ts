d5da6780271bfd0d95a95f5fafb6d779c053b061
Mochalov Maksim <mmochalov@usetech.ru>
2025-05-26 19:27:45 +0300

    fixes

--- src/screens/game.ts
+++ src/screens/game.ts
@@ -3,15 +3,21 @@ import { useWebcore } from '@/webcore'
 import type { Render } from '@/webcore/types'
 
 export const Game = (): Render => {
-  const { useMeasure } = useWebcore()
+  const { useMeasure, addEventResize } = useWebcore()
   const palette = useGradient()
   const { popEffect, render: renderPopEffects } = usePopEffect()
   const { popits, render: renderPopits } = usePopits({ palette, popEffect })
 
-  const pt = (popits[0].y - popits[0].r) / useMeasure().m
+  const splashPorps = { mfs: 0, dy: 0 }
 
-  const { splashEffect, render: renderSplashEffects } = useSplashEffect({ mfs: pt, dy: pt })
+  const { splashEffect, render: renderSplashEffects } = useSplashEffect(splashPorps)
   useGameplay({ popits, popEffect, splashEffect })
+
+  addEventResize(() => {
+    const pt = (popits[0].y - popits[0].r) / useMeasure().m
+    splashPorps.mfs = pt
+    splashPorps.dy = pt
+  })
 
   return () => {
     renderPopEffects()
