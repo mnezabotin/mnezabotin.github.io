a6219749dbd623c280333825614803bc02ea3b67
Mochalov Maksim <mmochalov@usetech.ru>
2025-06-13 15:50:06 +0300

    Updates

--- sw.js
+++ sw.js
@@ -71,23 +71,23 @@ self.addEventListener('install', (event) => {
 
 // при событии fetch, мы используем кэш, и только потом обновляем его данным с сервера
 self.addEventListener('fetch', function(event) {
-    // Мы используем `respondWith()`, чтобы мгновенно ответить без ожидания ответа с сервера.
-    event.respondWith(fromCache(event.request))
-    // `waitUntil()` нужен, чтобы предотвратить прекращение работы worker'a до того как кэш обновиться.
-    event.waitUntil(update(event.request))
+  // Мы используем `respondWith()`, чтобы мгновенно ответить без ожидания ответа с сервера.
+  event.respondWith(fromCache(event.request))
+  // `waitUntil()` нужен, чтобы предотвратить прекращение работы worker'a до того как кэш обновиться.
+  event.waitUntil(update(event.request))
 })
 
 function fromCache(request) {
-    return caches.open(CACHE).then((cache) =>
-        cache.match(request).then((matching) =>
-            matching || Promise.reject('no-match')
-        ))
+  return caches.open(CACHE).then((cache) =>
+    cache.match(request).then((matching) =>
+      matching || Promise.reject('no-match')
+    ))
 }
 
 function update(request) {
-    return caches.open(CACHE).then((cache) =>
-        fetch(request).then((response) =>
-            cache.put(request, response)
-        )
+  return caches.open(CACHE).then((cache) =>
+    fetch(request).then((response) =>
+      cache.put(request, response)
     )
+  )
 }
