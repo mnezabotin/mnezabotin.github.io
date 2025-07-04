4bdc370060efcd98f9f35bc417021df86dcd897d
Mochalov Maksim <mmochalov@usetech.ru>
2025-07-04 17:11:23 +0300

    Updates

--- sw.js
+++ sw.js
@@ -81,6 +81,10 @@ self.addEventListener('install', (event) => {
         './index.html',
         './manifest.json',
         './styles.css',
+
+        './js/gl-matrix-min.js',
+        './js/litegl.min.js',
+        './js/Canvas2DtoWebGL.js',
         './sw.js',
       ])
     )
